5c301da640ddad62619554862f0af0f881aaf13d
Shiro <sherifmohamed2004@gmail.com>
2021-07-11 16:14:13 +0200

    added the config and all the needed packges and libraries

---
+++ lib/config/palette.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class Palette{
+  static const Color lightblue = Color(0xFF00AEEF);
+  static const Color darkBlue = Color(0xFF00334c);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
