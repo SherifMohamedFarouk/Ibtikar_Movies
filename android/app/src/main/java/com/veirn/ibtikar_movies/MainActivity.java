2031ccc9ad6ac6bfbeacbd33c11f8a3cd4d7e85b
Shiro <sherifmohamed2004@gmail.com>
2021-07-11 15:58:52 +0200

    first commit

---
+++ android/app/src/main/java/com/veirn/ibtikar_movies/MainActivity.java
@@ -0,0 +1,6 @@
+package com.veirn.ibtikar_movies;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'IbtikarMovies',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PopularScreen(title: "IbtikarMovies",),
+    );
+  }
+}
